920c0d3738b812b4feb909fa9d923bb4ad4b4658
JustinCampbell <cmsjustin@gmail.com>
2011-04-05 21:35:31 -0400

    Cleaned up code, return Convore status to Pivotal

--- pivotal_convore_hook.rb
+++ pivotal_convore_hook.rb
@@ -4,22 +4,22 @@ require 'httparty'
 require 'crack'
 
 post '/:topic_id' do
+  credentials
+  payload = { "topic_id" => params[:topic_id], "message" => message }
+  options = { :basic_auth => { :username => @username, :password => @password }, :body => payload }
+  response = HTTParty.post "https://convore.com/api/topics/#{params[:topic_id]}/messages/create.json", options
+  status response.code
+end
+
+def credentials
   auth = Rack::Auth::Basic::Request.new(request.env)
-  username = auth.credentials[0]
-  password = auth.credentials[1]
+  @username = auth.credentials[0]
+  @password = auth.credentials[1]
+end
+
+def message
   input = Crack::XML.parse(request.body)
   description = input['activity']['description']
   url = input['activity']['stories']['story']['url']
-  message = "#{description} #{url}"
-  payload = { "topic_id" => params[:topic_id], "message" => message }
-  
-  options = {
-    :basic_auth => {
-      :username => username,
-      :password => password
-    },
-    :body => payload
-  }
-  
-  HTTParty.post "https://convore.com/api/topics/#{params[:topic_id]}/messages/create.json", options
+  "#{description} #{url}"
 end
