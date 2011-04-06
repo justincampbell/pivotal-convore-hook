9e778cedae7375159f3a17649a9f9f1d2073ee78
JustinCampbell <cmsjustin@gmail.com>
2011-04-05 21:42:22 -0400

    Pass credentials from route or basic auth

--- pivotal_convore_hook.rb
+++ pivotal_convore_hook.rb
@@ -4,6 +4,14 @@ require 'httparty'
 require 'crack'
 
 post '/:topic_id' do
+  special_sauce
+end
+
+post '/:topic_id/:username/:password' do
+  special_sauce
+end
+
+def special_sauce 
   credentials
   payload = { "topic_id" => params[:topic_id], "message" => message }
   options = { :basic_auth => { :username => @username, :password => @password }, :body => payload }
@@ -13,8 +21,8 @@ end
 
 def credentials
   auth = Rack::Auth::Basic::Request.new(request.env)
-  @username = auth.credentials[0]
-  @password = auth.credentials[1]
+  @username = params[:username] || auth.credentials[0]
+  @password = params[:password] || auth.credentials[1]
 end
 
 def message
